b5067ccd25978da3e65af80bfab40daad554a57f
Harsh Maheshwari <hrshdg8@gmail.com>
2021-09-24 17:31:58 -0700

    Add test logic for toArray methods

--- src/test/java/edu/sdsu/cs635/tree/DefaultTreeTest.java
+++ src/test/java/edu/sdsu/cs635/tree/DefaultTreeTest.java
@@ -3,6 +3,8 @@ package edu.sdsu.cs635.tree;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,6 +45,13 @@ public class DefaultTreeTest {
   public void getWhenIndexOutOfRange() {
     assertThrows(IndexOutOfBoundsException.class, () -> numberTree.get(-1));
     assertThrows(IndexOutOfBoundsException.class, () -> numberTree.get(numberTree.size()));
+  }
+
+  @Test
+  public void addAll() {
+    List<Integer> elementsToBeAdded = Arrays.asList(-2, -10, 6, 34, 2, -9);
+    numberTree.addAll(elementsToBeAdded);
+    assertEquals(-10, numberTree.get(0));
   }
 
 }
