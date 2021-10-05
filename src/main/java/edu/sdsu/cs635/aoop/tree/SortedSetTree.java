2aa54f3276af94d117c792585fdb9ff61bd96f4f
Harsh Maheshwari <hrshdg8@gmail.com>
2021-10-04 21:57:47 -0700

    clean up

--- src/main/java/edu/sdsu/cs635/aoop/tree/SortedSetTree.java
+++ src/main/java/edu/sdsu/cs635/aoop/tree/SortedSetTree.java
@@ -2,6 +2,9 @@ package edu.sdsu.cs635.aoop.tree;
 
 import java.util.SortedSet;
 
+/**
+ * Extension to the SortedSet Interface to add a provision for get method.
+ */
 public interface SortedSetTree<E> extends SortedSet<E> {
   E get(int index);
 }
